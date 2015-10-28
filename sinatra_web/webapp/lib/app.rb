90f9fdd7c99de7e8759e32ebdaa9d7bf992dc79c
jgm <kayak.jiang@gmail.com>
2015-10-28 20:49:06 +0800

    update

--- sinatra_web/webapp/lib/app.rb
+++ sinatra_web/webapp/lib/app.rb
@@ -1,8 +1,16 @@
 require "rubygems"
 require "sinatra"
 require "json"
+require "uri"
+require "redis"
+
+uri = URI.parse(ENV['DB_PORT'])
+redis = Redis.new(host: uri.host, port: uri.port)
+
 
 class App < Sinatra::Application
+
+  redis = Redis.new(host: 'db', port: '6379')
 
   set :bind, '0.0.0.0'
 
@@ -10,7 +18,13 @@ class App < Sinatra::Application
     "<h1>DockerBook Test Sinatra app</h1>"
   end
 
+  get '/json' do
+    params = redis.get("params")
+    params.to_json
+  end
+
   post '/json/?' do
+    redis.set "params", [params].to_json
     params.to_json
   end
 
